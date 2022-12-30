2c8cc929a7438a738347ddee5f3f85778cd5c87e
Aurélien Passion <aurelien.passion@gmail.com>
2022-12-30 20:54:18 +0100

    remove fees wallet from snapshots

--- interaction/snapshot.py
+++ interaction/snapshot.py
@@ -19,6 +19,8 @@ IGNORED_ADDRESS = [
     'erd1hmfwpvsqn8ktzw3dqd0ltpcyfyasgv8mr9w0qecnmpexyp280y8q47ca9d',
     # DAO
     'erd1ysc7l52t6pg0rkedwwwrld6zu55y2326dfx0maat68sapkl7c58q6kmvyt',
+    # Fees wallet
+    'erd1272et87h3sa7hlg5keuswh50guz2ngmd6lhmjxkwwu0ah6gdds5qhka964',
     # team (vested tokens)
     'erd1ysrm0kwsy3def8dmfwzrw68atepk0qadnu5sm2z6hv9e0ym2qwrsffuwjk',
     'erd19g9fa6tkqlvn5x2nuqvwlcmz943vpt5q3a92fkdsngu0zz62qpasyelgws',
