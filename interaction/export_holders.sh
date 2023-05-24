7db9d4687df3d1c884c3db313096d0871cbe77ab
Aurélien Passion <aurelien.passion@gmail.com>
2023-05-24 09:09:31 +0200

    add helpers scripts

---
+++ interaction/export_holders.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python snapshot.py export_holders
---
+++ interaction/register_holders.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python snapshot.py \
+    --sc_address erd1qqqqqqqqqqqqqpgqwkqnf30j7hj4r797kahr0p5t5nsksc8a73eqd732jd \
+    --keyfile ../../wallets/deployer.json \
+    register_holders
