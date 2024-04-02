1d3a4b146ab4ccc30b545873c31cd90aec82ecc8
Aurélien Passion <aurelien.passion@gmail.com>
2024-04-02 09:16:59 +0200

    update LP multipliers

--- interaction/snapshot.py
+++ interaction/snapshot.py
@@ -50,14 +50,14 @@ NFT_HOLDING_JEX_EQIV = 100_000
 
 # curl https://api.jexchange.io/pools/v3 | jq '.[] | "\(.lp_token.identifier) \(.usd_value_per_lp_token)"'
 # USD value of 1 LP token / 100 = multiplier
-LP_MULTIPLIERS = [("LPETHBTC-8b8a1f", 1.47),
-                  ("LPETHEGLD-bcb4ac", 1.28),
-                  ("LPJEXWETH-2a2e52", 1.45),
+LP_MULTIPLIERS = [("LPETHBTC-8b8a1f", 1.42),
+                  ("LPETHEGLD-bcb4ac", 1.21),
+                  ("LPJEXWETH-2a2e52", 1.44),
                   ("LPJEXUSDT-732142", 1.22),
-                  ("LPJEXWEGLD-2bccc4", 1.42),
-                  ("LPJEXBEE-a6fd37", 104.14),
-                  ("LPJEXRARE-518166", 1.39),
-                  ("LPJACKCOAT-d49dd4", 0.14),
+                  ("LPJEXWEGLD-2bccc4", 1.36),
+                  ("LPJEXBEE-a6fd37", 97.16),
+                  ("LPJEXRARE-518166", 1.30),
+                  ("LPJACKCOAT-d49dd4", 0.112),
                   ("LPUSDCUSDT-fd8cf1", 0.0103),
                   ("LPJEX3USD-25e943", 0.0101)]
 LPS_POOL_SIZE = 100_000_000 * 10**18
