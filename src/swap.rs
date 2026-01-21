6bc727399b9a4771d30e581c21c20bebae473c32
Aurélien Passion <aurelien.passion@gmail.com>
2026-01-21 10:03:56 +0100

    swap: only if wegld balance > 0

--- src/swap.rs
+++ src/swap.rs
@@ -12,17 +12,19 @@ pub trait SwapModule {
             0u64,
         );
 
-        // erd1qqqqqqqqqqqqqpgq7u4y0qle773mcelvnkapjv36pn2whzy36avs2qccja
-        let swap_sc_address = ManagedAddress::from(hex!(
-            "00000000000000000500f72a4783f9f7a3bc67ec9dba19323a0cd4eb8891d759"
-        ));
+        if wegld_balance > 1 {
+            // erd1qqqqqqqqqqqqqpgq7u4y0qle773mcelvnkapjv36pn2whzy36avs2qccja
+            let swap_sc_address = ManagedAddress::from(hex!(
+                "00000000000000000500f72a4783f9f7a3bc67ec9dba19323a0cd4eb8891d759"
+            ));
 
-        let payment = EsdtTokenPayment::new(wegld_identifier, 0u64, wegld_balance);
+            let payment = EsdtTokenPayment::new(wegld_identifier, 0u64, wegld_balance);
 
-        self.jexchange_lps_sc_proxy(swap_sc_address)
-            .swap_tokens_fixed_input(BigUint::from(2u64))
-            .with_esdt_transfer(payment)
-            .async_call_and_exit();
+            self.jexchange_lps_sc_proxy(swap_sc_address)
+                .swap_tokens_fixed_input(BigUint::from(2u64))
+                .with_esdt_transfer(payment)
+                .async_call_and_exit();
+        }
     }
 
     #[proxy]
